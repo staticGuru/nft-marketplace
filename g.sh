a6f1fdf5aff3fa234d251d567ae004f388c070cc
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-07 10:47:42 +0530

    cleanup the initial react project

---
+++ g.sh
@@ -0,0 +1,8 @@
+git add .
+
+echo 'Enter the commit message:'
+read commitMessage
+
+git commit -m "$commitMessage"
+
+git push origin main
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+// import './App.css'
+
+function App() {
+
+  return (
+    <div>
+     NFT
+    </div>
+  )
+}
+
+export default App
