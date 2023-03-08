73ad8387a8a7d8457af8b10fa051b859d4ac7e20
Guruvignesh M <vigneshguru274@gmail.com>
2023-03-08 16:44:48 +0530

    add the context sections

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { StateContextProvider } from './context';
+import { StateContextProvider } from './context/index';
 import './index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <React.StrictMode>
